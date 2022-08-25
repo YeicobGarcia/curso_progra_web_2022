db3adb3e7b9cb0fbaeb00633efcf13a5f6edc984
YeicobGarcia <jacobgarcia.jm@mail.com>
2022-08-25 01:25:10 -0600

    Armado completo de Sidebar

--- grilla-bootstrap/index.js
+++ grilla-bootstrap/index.js
@@ -3,10 +3,6 @@ $(function () {
 })
 
 /* SideBar */
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
-});
 $("#menu-toggle-2").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled-2");
