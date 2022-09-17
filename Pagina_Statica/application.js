8d340050118fe598af8bc172839c2c6587ad427a
YeicobGarcia <jacobgarcia.jm@mail.com>
2022-09-16 22:43:17 -0600

    Funcionalidad del reproductor resuelta

--- Pagina_Statica/application.js
+++ Pagina_Statica/application.js
@@ -1,16 +1,65 @@
-
-
-function mostraralerta(){
-  alert('Se hizo Click en el titulo desde una function')
+function presionar() {
+  if (!medio.paused && !medio.ended) {
+    medio.pause();
+    document.querySelector(".fa-play").style.display = "block";
+    document.querySelector(".fa-pause").style.display = "none";
+    window.clearInterval(bucle);
+  } else {
+    medio.play();
+    document.querySelector(".fa-play").style.display = "none";
+    document.querySelector(".fa-pause").style.display = "block";
+    bucle = setInterval(estado, 1000);
+  }
 }
 
-function mostrarMensaje(){
-  alert('Se hizo click')
+function estado() {
+  if (!medio.ended) {
+    var total = parseInt(medio.currentTime * maximo / medio.duration);
+    progreso.style.width = total + 'px';
+  } else {
+    progreso.style.width = '0px';
+    bucle.setInterval(bucle);
+  }
 }
 
-function haceclick(){
-  var elemento = document.getElementById('contenido');
-  elemento.addEventListener('mouseover', mostrarMensaje, false);
+function iniciar() {
+  maximo = 600;
+  medio = document.getElementById("medio");
+  reproducir = document.getElementById("reproducir");
+  barra = document.getElementById("barra");
+  retroceder = document.getElementById("retroceder");
+  adelantar = document.getElementById("adelantar");
+  progreso = document.getElementById("progreso");
+  expandir = document.getElementById("botones2");
+
+  reproducir.addEventListener("click", presionar, false);
+  barra.addEventListener("click", mover, false);
+  retroceder.addEventListener("click", rewind, false);
+  adelantar.addEventListener("click", forward, false);
+  expandir.addEventListener("click", fullscreen, false);
+
 }
 
-window.addEventListener('load', haceclick, false);
+function rewind(e) {
+  medio.currentTime = medio.currentTime - ((medio.duration / 100) * 5);
+}
+
+function forward(e) {
+  medio.currentTime = medio.currentTime + ((medio.duration / 100) * 5);
+}
+
+function fullscreen(e) {
+  e.preventDefault();
+  medio.requestFullscreen();
+}
+
+function mover(e) {
+  if (!medio.paused && !medio.ended) {
+    var ratonX = e.pageX - barra.offsetLeft;
+    var nuevoTiempo = ratonX * medio.duration / maximo;
+    medio.currentTime = nuevoTiempo;
+    progreso.style.width = ratonX + 'px';
+  }
+}
+
+window.addEventListener('load', iniciar, false);
